a2cd7460451b52cde749e1ea0ee7aa7180af3d6f
ohkillah <mephje@gmail.com>
2023-03-07 18:25:30 +0600

    last

---
+++ star/src/components/app/app.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react'
+
+import Header from '../header'
+import RandomPlanet from '../random-planet'
+
+import './app.css'
+import ErrorIndicator from "../error-indicator"
+import ErrorBoundary from "../error-boundary"
+
+import { SwapiServiceProvider } from '../swapi-service-context'
+import SwapiService from "../../services/swapi-service"
+import { PeoplePage, PlanetsPage, StarshipsPage } from "../pages"
+
+export default class App extends Component{
+
+    state = {
+        selectedItem: null,
+        hasError: false,
+        swapiService: new SwapiService()
+    }
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({ hasError: true })
+    }
+
+    render() {
+
+        if (this.state.hasError) {
+            return <ErrorIndicator />
+        }
+
+        return (
+            <ErrorBoundary>
+                <SwapiServiceProvider value={this.state.swapiService} >
+                    <div className="stardb-app">
+
+                        <Header/>
+                        <RandomPlanet />
+
+                        <PeoplePage />
+                        <PlanetsPage />
+                        <StarshipsPage />
+
+                    </div>
+                </SwapiServiceProvider>
+            </ErrorBoundary>
+        )
+    }
+}
